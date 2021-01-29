3905a141159c9e96f770029281eda5235c0b7e25
milOrd <9x9willbe18@gmail.com>
2021-01-29 22:46:37 +0300

    New commit

--- java/script.js
+++ java/script.js
@@ -1,6 +1,23 @@
 "use strick";
+const age = +prompt('Сколко тебя лет?', "");
 
-const a = prompt('Сколко тебе лет?', "");
-console.log(a);
-const b = prompt('Сколко тебе лет?', "");
-console.log(b);
+const profile = {
+    Ages: age,
+    intr: [],
+    FnameLname:{}
+
+};
+
+const int1 = prompt('Скажи свое хоби?', ""),
+      int2 = prompt('Скажи что ты делаешь на выходных?', ""),
+      Fname = prompt('Какая у тебя фамилиия?', ''),
+      Lname = prompt('Как тебя зовут?', '');
+
+profile.intr[0] = [int1],
+profile.intr[1] = [int2],
+
+
+profile.FnameLname['FirstName'] = Fname,
+profile.FnameLname['LastName'] = Lname;
+console.log(profile);
+
